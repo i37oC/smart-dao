74637beef5917a4ebce7eebba0bda0dcec74f084
xuyuanyang <xuyuanyang@loonxi.com>
2017-05-29 19:22:11 +0800

    xyy

---
+++ smart-dao-example/src/main/java/org/xyy/smart/dao/example/SmartDaoExampleApplication.java
@@ -0,0 +1,12 @@
+package org.xyy.smart.dao.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDaoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDaoExampleApplication.class, args);
+	}
+}
